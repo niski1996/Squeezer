1fee02acd8c24c9a44879a9bc269eee819e1d491
niski1996 <niski1996@interia.pl>
2025-06-20 00:15:45 +0200

    Autogenerated commit

--- script/script.py
+++ script/script.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 import argparse
 import logging
-from command_executor import CommandExecutor
+from command_executor import DefaultCommandExecutor
 from check_and_gen_fun import *
 from git_function import *
 
@@ -18,7 +18,6 @@ def setup_logger(verbose: bool = False) -> logging.Logger:
     formatter = logging.Formatter('%(levelname)s: %(message)s')
     ch.setFormatter(formatter)
     
-    # Avoid adding multiple handlers if setup_logger is called multiple times
     if not logger.hasHandlers():
         logger.addHandler(ch)
     
@@ -39,42 +38,44 @@ def main():
     args = parser.parse_args()
 
     logger = setup_logger(args.verbose)
-    executor = CommandExecutor(logger=logger)
+    executor = DefaultCommandExecutor(logger=logger)
 
     current_dir = Path(".").resolve()
     logger.debug(f"Checking directory: {current_dir}")
 
-    if check_if_current_is_git_repo(executor):
+    # Updated function calls with both runner and logger
+    if check_if_current_is_git_repo(executor, logger):
         logger.info(f"Directory '{current_dir}' IS a Git repository.")
     else:
         logger.error(f"Directory '{current_dir}' is NOT a Git repository.")
         sys.exit(1)
 
-    actual_branch = get_actual_git_branch(executor)
+    actual_branch = get_actual_git_branch(executor, logger)
     logger.info(f"Current directory: {current_dir}")
     logger.info(f"Current branch: {actual_branch}")
 
-    parent_branch = get_direct_branch_parent(executor)
+    parent_branch = get_direct_branch_parent(executor, logger)
     logger.info(f"Parent branch: {parent_branch}")
+    
     if not parent_branch:
         logger.error(f"{actual_branch} has no parent branch. Check if it isn't a root branch.")
         sys.exit(1)
+        
     target_branch = actual_branch.rstrip(" !\n")
     auxiliary_branch = f"{target_branch}-{AUXILIARY_BRANCH_SUFFIX}"
 
-    target_branch_exists = check_if_branch_exists(executor, target_branch)
-    auxiliary_branch_exists = check_if_branch_exists(executor, auxiliary_branch)
+    target_branch_exists = check_if_branch_exists(executor, logger, target_branch)
+    auxiliary_branch_exists = check_if_branch_exists(executor, logger, auxiliary_branch)
+    
     if not target_branch_exists or not auxiliary_branch_exists:
         if not target_branch_exists:
             logger.error(f"Target branch '{target_branch}' does not exist.")
-        
-    
-    
-
+        if not auxiliary_branch_exists:
+            logger.error(f"Auxiliary branch '{auxiliary_branch}' does not exist.")
+        sys.exit(1)
 
     if args.message:
         logger.info(f"Message: {args.message}")
-
 
 if __name__ == "__main__":
     main()
